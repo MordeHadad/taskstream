a15216df9c76c946f224c0641fe417e2e9d3cf63
MordeHadad <mordechai.hadad01+jobs@gmail.com>
2025-03-25 14:13:13 +0200

    added accel buffering for reverse proxies

--- backend/src/sse.ts
+++ backend/src/sse.ts
@@ -7,6 +7,7 @@ export const setupSSEHandler = (req: Request, res: Response) => {
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
+    res.setHeader('X-Accel-Buffering', 'no');
 
     res.write(`data: ${JSON.stringify({ log: 'Connected to stream', status: 'connected' })}\n\n`);
 
